1a9b955016235bef024a5b3679784c83cc2ba853
Christian Maher <cmaher@acm.org>
2012-02-08 13:09:29 -0500

    added support for preparing the robot (turning it on and readying it)

--- src/biscuit.c
+++ src/biscuit.c
@@ -1,8 +1,33 @@
 #include "biscuit.h"
 
-void bisc_init() {
+void bisc_prepare() {
+    bisc_led_off(BISC_LED_BOTH);
+
+    //TODO: when we handle interrupt stuff, it will go here
+
+    bisc_power_on();
     bisc_start();
-    bisc_setMode(BISC_MODE_SAFE);
+    bisc_mode_safe();
+}
+
+void bisc_power_on() {
+    //check if the power is already on
+    if(!BISC_PWR_IS_ON) {
+        //loop until the power is on
+        while(!BISC_PWR_IS_ON) {
+            //a power tranisition happens when the powr toggles low to high
+            //delays are included to let the create notice the change
+            BISC_PWR_TOGGLE_LOW();
+            delayMs(100);
+            BISC_PWR_TOGGLE_HIGH();
+            delayMs(100);
+        }
+        //toggle power back to low
+        BISC_PWR_TOGGLE_LOW();
+
+        //delay for create bootloader
+        delayMs(2000);
+    }
 }
 
 void bisc_mode_passive() {
@@ -17,12 +42,12 @@ void bisc_mode_full() {
     bisc_setMode(BISC_MODE_FULL);
 }
 
-void bisc_ledOn(uint8_t led, uint8_t color, uint8_t intensity) {
+void bisc_led_on(uint8_t led, uint8_t color, uint8_t intensity) {
     bisc_led(led, color, intensity);
 }
 
 
-void bisc_ledOff(uint8_t led) {
+void bisc_led_off(uint8_t led) {
     // Power LED = 0x08
     // Advance LED = 0x02
     // Both power and advance = 0x0A
--- src/biscuit.h
+++ src/biscuit.h
@@ -2,11 +2,31 @@
 #define __BISCUIT_
 
 #include "biscuit_core.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdlib.h>
 
 /*
- * Starts the Open Interface and sets the mode to BISC_MODE_SAFE
+ * Power related macros
+ *
+ * Power High bit is 0x80 on PORTD
+ * Power Sense bit is 0x20 on PINB
  */
-void bisc_init();
+#define BISC_PWR_HIGH               0x80
+#define BISC_PWR_SENSE              0x20
+#define BISC_PWR_TOGGLE_HIGH()      (PORTD |= BISC_PWR_HIGH)
+#define BISC_PWR_TOGGLE_LOW()       (PORTD &= ~BISC_PWR_HIGH)
+#define BISC_PWR_IS_ON              (PINB & BISC_PWR_SENSE)
+
+/*
+ * Prepares the Create to execute programs
+ */
+void bisc_prepare();
+
+/*
+ * Powers on the Create
+ */
+void bisc_power_on()
 
 /*
  * Sets the operating mode of Create to passive
@@ -30,13 +50,13 @@ void bisc_mode_full();
  * color: integer 0-255 from green to red
  * intensity: integer 0-255 from 0% to 100%
  */
-void bisc_ledOn(uint8_t led, uint8_t color, uint8_t intensity);
+void bisc_led_on(uint8_t led, uint8_t color, uint8_t intensity);
 
 /*
  * description
  * 
  * led: 
  */
-void bisc_ledOff(uint8_t led);
+void bisc_led_off(uint8_t led);
 
 #endif
